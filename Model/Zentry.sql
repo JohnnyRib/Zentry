905c75fd91f8dce6351becae02360506d13e7413
Jeremy1012-esp <jeremy10juninho@gmail.com>
2026-05-10 22:24:37 +0200

    DataBase, create table user

--- Model/Zentry.sql
+++ Model/Zentry.sql
@@ -8,3 +8,12 @@ CREATE USER 'Zentry_team'@'localhost' IDENTIFIED BY 'Zentry687';
 GRANT ALL PRIVILEGES ON Zentry.* TO 'Zentry_team'@'localhost';
 
 FLUSH PRIVILEGES;
+
+-- Estructura de la tabla user
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `role` ENUM('Cliente', 'Promotor') NOT NULL DEFAULT 'Cliente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
