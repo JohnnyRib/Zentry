1e46501df6b03007c22f8cb5b0565009476a998d
Johnny <johnny.rib15@gmail.com>
2026-05-20 08:26:31 +0200

    commit prueba

--- Controler/controlerUser.php
+++ Controler/controlerUser.php
@@ -5,6 +5,9 @@ Inicio de sesión y control de acceso para el login,
 registro y logout de usuarios.
 =========================================*/
 
+
+//commit prueba
+
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
