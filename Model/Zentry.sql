2013c1d7c76f4951e92ba0d378ed5a79c07e9837
Johnny <johnny.rib15@gmail.com>
2026-05-21 09:43:37 +0200

    tables sql

--- Model/Zentry.sql
+++ Model/Zentry.sql
@@ -24,3 +24,18 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- ENGINE=InnoDB: Garantiza integridad referencial y recuperación de errores.
 -- CHARSET=utf8mb4: Soporte completo para caracteres especiales y emojis.
 
+DROP TABLE IF EXISTS `eventos`;
+-- Elimina la tabla eventos si ya existe para evitar conflictos al crearla nuevamente.
+
+CREATE TABLE IF NOT EXISTS `eventos` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL,
+    fecha DATE NOT NULL,
+    ubicacion VARCHAR(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Configuración de la Tabla de Eventos
+-- ENGINE=InnoDB: Garantiza integridad referencial y recuperación de errores.
+-- CHARSET=utf8mb4: Soporte completo para caracteres especiales y emojis.
+
+
